d950284f115572063e31c6a038f1af2542db67cf
zxl0526 <851450219@qq.com>
2020-09-21 21:02:44 +0800

    should_Return_FlagType

--- src/test/java/args/SchemaTest.java
+++ src/test/java/args/SchemaTest.java
@@ -1,6 +1,17 @@
 package args;
+import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+import static org.junit.Assert.assertEquals;
 
 public class SchemaTest {
-
+    @Test
+    public void should_Return_FlagType(){
+        Set<FlagSchema> flagSchemaSet = new HashSet<FlagSchema>();
+        flagSchemaSet.add(new FlagSchema("l", ValueType.BOOLEAN));
+        Schema schema = new Schema(flagSchemaSet);
+        String type = schema.getTypeOf("l");
+        assertEquals("java.lang.Boolean",type);
+    }
 }
